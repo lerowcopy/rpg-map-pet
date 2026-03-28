501c4c7f6d0cdc9bf9fa429569842bfa62052204
DESKTOP-RMPSLBG\lerow <lerow77@gmail.com>
2026-03-29 02:12:11 +0300

    feat: обновить экран посещённых меток

--- app/src/main/java/com/example/rpg_map_pet/presentation/visited/VisitedLandmarksScreen.kt
+++ app/src/main/java/com/example/rpg_map_pet/presentation/visited/VisitedLandmarksScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.*
@@ -44,7 +45,7 @@ fun VisitedLandmarksScreen(
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Назад"
                         )
                     }
